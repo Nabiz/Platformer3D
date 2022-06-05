ecf292c5833b3dfb3b6fdae5fcc313c01719f032
Nabiz <dawcio.gwizdz@gmail.com>
2022-06-05 21:10:45 +0200

    Add animation to kickBouncer

--- Platformer3D/Assets/Scripts/Trampoline/KickBounce.cs
+++ Platformer3D/Assets/Scripts/Trampoline/KickBounce.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class KickBounce : MonoBehaviour
 {
     [SerializeField] float jumpHeight = 5.0f;
+    private Animator animator;
+    private AudioSource audio;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("PlayerKick"))
         {
             PlayerController player = other.GetComponentInParent<PlayerController>();
             player.SetJumpVelocity(jumpHeight);
+            animator.SetTrigger("Kicked");
+            audio.Play();
         }
     }
 }
