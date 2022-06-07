5fd547cfd77f1b02c3bf0b62bc14e9350f23008c
Nabiz <dawcio.gwizdz@gmail.com>
2022-06-07 19:43:17 +0200

    assign levels to buttons, fix lightning when change scene

--- Platformer3D/Assets/Scripts/Menu/MenuController.cs
+++ Platformer3D/Assets/Scripts/Menu/MenuController.cs
@@ -10,12 +10,19 @@ public class MenuController : MonoBehaviour
     [Header("Levels To Load")]
 
     public string _level1;
+    public string _level2;
     public string _level3;
     public string _level4;
+    public string _level5;
 
     public void Level1_run()
     {
         SceneManager.LoadScene(_level1);
+    }
+
+    public void Level2_run()
+    {
+        SceneManager.LoadScene(_level2);
     }
 
     public void Level3_run()
@@ -26,6 +33,11 @@ public class MenuController : MonoBehaviour
     public void Level4_run()
     {
         SceneManager.LoadScene(_level4);
+    }
+
+    public void Level5_run()
+    {
+        SceneManager.LoadScene(_level5);
     }
 
     public void ExitButton()
